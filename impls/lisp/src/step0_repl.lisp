c1a971e678d721b80ea703d1e8453727af73696b
Ulrich <lisp@ulrivo.de>
2021-01-05 22:27:22 +0100

    working on first repl

--- impls/lisp/src/step0_repl.lisp
+++ impls/lisp/src/step0_repl.lisp
@@ -1,12 +1,25 @@
+;;;  (load "step0_repl.asd")
+;;;  Omit the load if there is a link:
+;;;   ln -s ~/dev/lisp/mal/impls/lisp ~/common-lisp/mal
+
+;;;  (ql:quickload :step0_repl)
+
+;;;   ,in-package :mal
+
 (defpackage :mal
   (:use :common-lisp)
-  (:import-from :uiop
-   :getenv)
   (:import-from :cl-readline
    :readline)
   (:export :main))
 
 (in-package :mal)
 
+(defun repl (input)
+  input)
+
 (defun main ()
-  (princ "hello welt, lieber Ulrich"))
+  (loop 
+    (princ "user> ")
+    (force-output)
+    (princ (repl (read-line)))
+    (terpri)))
